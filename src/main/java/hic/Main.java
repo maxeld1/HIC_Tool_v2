23b12445a92610a9b2c20086f9e4529179544cf3
maxeld1 <148884995+maxeld1@users.noreply.github.com>
2024-04-19 00:58:15 -0400

    working on HIC menu

--- src/main/java/hic/Main.java
+++ src/main/java/hic/Main.java
@@ -1,14 +1,21 @@
 import hic.datamanagement.FileReader;
 import hic.logging.HICExcelLogger;
 import hic.processor.Processor;
+import hic.ui.UserInterface;
 import hic.util.HICData;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+/**
+ * HIC Program for set up
+ * @Author: Max Eldabbas
+ */
 public class Main {
     public static void main(String[] args) {
+
+        UserInterface userInterface = new UserInterface();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -22,37 +29,8 @@ public class Main {
 
         List<HICData> hicData =  fileReader.parseFile("HIC_FILE.txt"); //parse the HIC file
 
-        //System.out.println(hicData); //print out HIC data records
+        userInterface.mainMenu(hicData, donor);
 
-        List<Double> maxAndMinRequests = processor.printHICSummary(hicData); //get the max and min requests
-
-        System.out.println();
-
-        processor.calculateApheresisNeeded(maxAndMinRequests); //calculate the amount of apheresis needed
-
-        System.out.println();
-
-        // Export HIC data to excel sheet (unsorted)
-        hicExcelLogger.logHICData(hicData, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\Unsorted_HICdoc.xlsx", false);
-
-        // Sort the hicData by cell type and date/time
-        processor.sortByCellTypeAndDateTime(hicData);
-
-        // Get the CD4 and CD8 cell types in a separate list
-        List<HICData> cd4Cd8List = processor.getCD4CD8CellRecords(hicData);
-
-        // Get the other cell types in a separate list
-        List<HICData> otherCellTypesList = processor.getOtherCellTypeRecords(hicData);
-
-        // Export the sorted HIC data to excel sheet
-        hicExcelLogger.logHICData(hicData, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\Sorted_Labeled_HICDoc.xlsx", true);
-
-        // Export HIC data to labels word doc
-        hicExcelLogger.exportToWord(cd4Cd8List, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\HIC_Program_Label_Template2.docx", "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\CD4CD8_Labels.docx", donor);
-
-
-        // Export HIC data to labels word doc
-        hicExcelLogger.exportToWord(otherCellTypesList, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\HIC_Program_Label_Template2.docx", "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\OTHERCellTypes_Labels.docx", donor);
 
 
 
--- src/main/java/hic/ui/UserInterface.java
+++ src/main/java/hic/ui/UserInterface.java
@@ -1,197 +1,147 @@
 package hic.ui;//package ui;
-//
-//import edu.upenn.cit594.datamanagement.FileReader;
-//import edu.upenn.cit594.processor.AverageMarketValueProcessor;
-//import edu.upenn.cit594.processor.AverageTotalLivableAreaProcessor;
-//import edu.upenn.cit594.processor.Processor;
-//import edu.upenn.cit594.util.CovidData;
-//import edu.upenn.cit594.util.PopulationData;
-//
-//import java.util.List;
-//import java.util.Objects;
-//import java.util.Scanner;
-//
-//public class UserInterface {
-//
-//    private FileReader fileReader;
-//    private Processor processor;
-//
-//    public UserInterface() {
-//        fileReader = FileReader.getInstance();
-//        processor = new Processor(fileReader);
-//    }
-//
-//    //TODO: Adi
-//    public void mainMenu(String covidArg, String propertyArg, String populationArg, String logArg) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//        boolean quit = false;
-//
-//        String keyword = null;
-//        if (covidArg.contains(".csv")) {
-//            keyword = "covidData";
-//        }
-//
-//        while (!quit) {
-//            printMainMenu();
-//            int choice = scanner.nextInt();
-//
-//            switch (choice) {
-//                case 0:
-//                    //exitProgram();
-//                    quit = true;
-//                    break;
-//                case 1:
-//                    showAvailableActions();
-//                    break;
-//                case 2:
-//                    processor.getTotalPopulationForZip(populationArg);
-//                    break;
-//                case 3:
-//                    getVaccinations(covidArg, populationArg, keyword);
-//                    break;
-//                case 4:
-//                    showAvgMarketValZip(propertyArg);
-//                    break;
-//                case 5:
-//                    showAvgLivableAreaZip(propertyArg);
-//                    break;
-//                case 6:
-//                    showMarketValProperties(propertyArg, populationArg);
-//                    break;
-//                case 7:
-//                    customAction();
-//                    break;
-//                default:
-//                    System.out.println("Invalid selection. Please try again.\n");
-//            }
-//        }
-//
-//
-//
-//    }
-//
-//    //TODO: All of these methods are already created in the processor package, but they need to be scanner friendly
-//
-//
-//    private void printMainMenu() {
-//
-//        System.out.println("\nPlease choose from the list: ");
-//        System.out.println();
-//        System.out.println("0. Exit the program");
-//        System.out.println("1. Show available actions");
-//        System.out.println("2. Show the total population for all ZIP Codes");
-//        System.out.println("3. Show the total vaccinations per capita for each ZIP Code for a specified date");
-//        System.out.println("4. Show the average market value for properties in a specified ZIP Code");
-//        System.out.println("5. Show the average total livable area for properties in a specified ZIP Code");
-//        System.out.println("6. Show the total market value of properties, per capita, for a specified ZIP Code");
-//        System.out.println("7. Custom feature (TODO)");
-//    }
-//
-//    //TODO: Adi
-//    private void exitProgram() {
-//    }
-//
-//    //TODO: Adi
-//    private void showAvailableActions() {
-//    }
-//
-//    private void getTotalPopulation() {
-//
-//    }
-//
-//    //TODO: Adi
-//    private void getVaccinations(String covidArg, String populationArg, String keyword) {
-//
-//        List<CovidData> covidDataList = fileReader.parseFile(covidArg, keyword);
-//
-//        List<PopulationData> populationDataList = fileReader.parseFile(populationArg, "populationData");
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        boolean partial = false;
-//
-//        System.out.println("Please specify whether you want to inspect 'partial' or 'full' vaccinations: ");
-//
-//        String vaxChoice = scanner.next();
-//
-//        if (Objects.equals(vaxChoice, "partial")) {
-//            partial = true;
-//        } else if (Objects.equals(vaxChoice, "full")) {
-//            partial = false;
-//        } else {
-//            System.out.println("Please pick a valid choice.");
-//            return;
-//        }
-//
-//        System.out.println("Please enter a date you want to inspect in the format YYYY-MM-DD: ");
-//
-//        String dateChoice = scanner.next();
-//
-//        processor.getVaccinationsPerCapita(covidDataList, populationDataList, dateChoice, partial);
-//
-//    }
-//
-//    //TODO: Adi
-//    private void showAvgMarketValZip(String propertyArg) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Please enter a 5-digit ZIP code to investigate:");
-//
-//        String zipCode = scanner.next();
-//
-//        AverageMarketValueProcessor averageMarketValueProcessor = new AverageMarketValueProcessor(fileReader);
-//
-//        averageMarketValueProcessor.processAverage(propertyArg, zipCode);
-//    }
-//
-//    //TODO: Adi
-//    private void showAvgLivableAreaZip(String propertyArg) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Please enter a 5-digit ZIP code to investigate:");
-//
-//        String zipCode = scanner.next();
-//
-//        AverageTotalLivableAreaProcessor averageTotalLivableAreaProcessor = new AverageTotalLivableAreaProcessor(fileReader);
-//
-//        averageTotalLivableAreaProcessor.processAverage(propertyArg, zipCode);
-//    }
-//
-//    //TODO: Adi
-//    private void showMarketValProperties(String propertyArg, String populationArg) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Please enter a 5-digit ZIP code to investigate:");
-//
-//        String zipCode = scanner.next();
-//
-//        processor.getTotalMarketValuePerCapita(propertyArg, populationArg, zipCode);
-//
-//    }
-//
-//    //TODO: Adi
-//    private void customAction() {
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//}
+
+import hic.datamanagement.FileReader;
+import hic.logging.HICExcelLogger;
+import hic.processor.Processor;
+import hic.util.HICData;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private FileReader fileReader;
+    private Processor processor;
+    HICExcelLogger hicExcelLogger;
+
+    public UserInterface() {
+        fileReader = FileReader.getInstance();
+        processor = new Processor(fileReader);
+        hicExcelLogger = HICExcelLogger.getInstance();
+    }
+
+    public void mainMenu(List<HICData> hicData, String donor) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+
+        while (!quit) {
+            printMainMenu();
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 0:
+                    exitProgram();
+                    quit = true;
+                    break;
+                case 1:
+                    getHICSummary(hicData);
+                    break;
+                case 2:
+                    exportToExcelUnsorted(hicData);
+                    break;
+                case 3:
+                    exportToExcelSorted(hicData);
+                    break;
+                case 4:
+                    makeLabels(hicData, donor);
+                    break;
+                case 5:
+                    performAllActions();
+                    break;
+                default:
+                    System.out.println("Invalid selection. Please try again.\n");
+            }
+        }
+
+
+
+    }
+
+
+    private void printMainMenu() {
+        System.out.println("\n=======================");
+        System.out.println("      HIC Menu: ");
+        System.out.println("=======================\n");
+        System.out.println("0. Exit the program");
+        System.out.println("1. Get HIC Summary and Calculate Apheresis");
+        System.out.println("2. Export unsorted HIC data to Excel file");
+        System.out.println("3. Export sorted HIC data to Excel file");
+        System.out.println("4. MAKE LABELS");
+        System.out.println("5. ALL OF THE ABOVE (Will run everything)");
+    }
+
+
+    private void exitProgram() {
+        System.out.println("Exiting the program...");
+        System.exit(0);
+    }
+
+
+    private void getHICSummary(List<HICData> hicData) {
+
+        List<Double> maxAndMinRequests = processor.printHICSummary(hicData); //get the max and min requests
+
+        processor.calculateApheresisNeeded(maxAndMinRequests); //calculate the amount of apheresis needed
+    }
+
+
+
+    private void exportToExcelUnsorted(List<HICData> hicData) {
+
+        // Export HIC data to excel sheet (unsorted)
+        hicExcelLogger.logHICData(hicData, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\Unsorted_HICdoc.xlsx", false);
+
+    }
+
+
+    private void exportToExcelSorted(List<HICData> hicData) {
+
+
+        // Sort the hicData by cell type and date/time
+        processor.sortByCellTypeAndDateTime(hicData);
+
+        // Export the sorted HIC data to excel sheet
+        hicExcelLogger.logHICData(hicData, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\Sorted_Labeled_HICDoc.xlsx", true);
+
+    }
+
+
+    private void makeLabels(List<HICData> hicData, String donor) {
+
+        // Get the CD4 and CD8 cell types in a separate list
+        List<HICData> cd4Cd8List = processor.getCD4CD8CellRecords(hicData);
+
+        // Get the other cell types in a separate list
+        List<HICData> otherCellTypesList = processor.getOtherCellTypeRecords(hicData);
+
+        // Export HIC data to labels word doc
+        hicExcelLogger.exportToWord(cd4Cd8List, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\HIC_Program_Label_Template2.docx", "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\CD4CD8_Labels.docx", donor);
+
+
+        // Export HIC data to labels word doc
+        hicExcelLogger.exportToWord(otherCellTypesList, "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\HIC_Program_Label_Template2.docx", "C:\\Users\\maxel\\IdeaProjects\\HIC_Tool_v2\\OTHERCellTypes_Labels.docx", donor);
+
+
+
+    }
+
+    private void performAllActions() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
